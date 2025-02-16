d72ca74f1063359e2c314b580c0a4f54c87d0c58
Radman <97783226+radmanon@users.noreply.github.com>
2025-02-16 09:38:31 -0800

    update server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mysql = require("mysql");
 
 const con = mysql.createConnection({
     host: process.env.DB_HOST,
+    port: 25060,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
