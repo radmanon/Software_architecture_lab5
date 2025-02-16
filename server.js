d25057e4a7fd0e1acb31ee38c2c40516ebc86f79
Radman <97783226+radmanon@users.noreply.github.com>
2025-02-16 03:57:53 -0800

    update server.js

--- server.js
+++ server.js
@@ -3,15 +3,15 @@ const mysql = require("mysql");
 
 
 const con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "lab5"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 con.connect(function(err){
     if (err) throw err;
-    console.log("Connected!");
+    console.log("Connected to database!");
 })
 
 con.query(
